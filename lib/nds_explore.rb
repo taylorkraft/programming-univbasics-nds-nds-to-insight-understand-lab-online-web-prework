4d815a863a676ee2b2811a87fb10a21bcc2a07fd
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-03 16:51:38 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,8 +8,8 @@ end
 
 def print_first_directors_movie_titles
   index = 0
-  while index < directors_database[:movies].length do
-     puts "#{directors_database[:movies][index]}\n"
+  while index < directors_database[0][:movies].length do
+     puts "#{directors_database[0][:movies][index][:title]}\n"
     index += 1
   end
 end
